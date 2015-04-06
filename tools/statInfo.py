2e4a518279ab1a486e4fdf1b1c1cb4e3f7540bd4
zhaoming <zhaoming@zhaomings-MacBook-Pro.local>
2015-04-07 00:32:24 +0800

    commit getCharCode.py

--- tools/statInfo.py
+++ tools/statInfo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'zhaoming'
 
-def statInfo(lst=[]):
+def statInfo_orig(lst=[]):
     data={}
     for item in lst:
         try:
@@ -10,7 +10,16 @@ def statInfo(lst=[]):
             data[item]=1
     return data
 
+def statInfo(lst=[]):
+    from collections import Counter
+    return Counter(lst)
+
 if __name__=='__main__':
     from cutWord import cutWordFromFile
     lst=cutWordFromFile('../corpus/test.txt')
+    from collections import Counter
     data=statInfo(lst)
+    sortLst=sorted(data,reverse=False)
+    print data
+    # for item in sortLst:
+    #     print item,data[item]
