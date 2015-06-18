97b86e749574949a702327b939458383148eae35
zmming <zhao.mming@gmail.com>
2015-06-18 10:13:52 +0800

    fix bug

--- tools/getFeatureWord.py
+++ tools/getFeatureWord.py
@@ -5,19 +5,21 @@ import jieba
 import jieba.analyse
 
 def getFeatureWord(content,topK):
-    return jieba.analyse.extract_tags(content, topK=topK, withWeight=True)
+    return jieba.analyse.extract_tags(content, topK=topK)
 
 def getFeaturWordByTextRank(con, topK):
     return jieba.analyse.textrank(con, topK=topK, withWeight=True)
 
 
 if __name__=='__main__':
+    import sys
+    sys.path.append('..')
     from baseLib.getCharCode import getCharType
     import codecs
-    charType=getCharType('../corpus/test3.txt')
+    charType=getCharType('../corpus/test.txt')
     #with codecs.open('../corpus/test2.txt','r',charType) as fin:
     #    con=fin.read()
-    with open('../corpus/test3.txt') as fin:
+    with open('../corpus/test.txt') as fin:
         con=fin.read()
     word_lst=getFeatureWord(con.decode(charType,'ignore').encode('utf-8'), 100)
     #print '/'.join(word_lst)
